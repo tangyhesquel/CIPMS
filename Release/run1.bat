a6e629a3dd873b176981b328f2872cffe93fe8f2
tangyh <tangyh@esquel.com>
2018-06-25 01:07:45 +0800

    1

--- Release/run1.bat
+++ Release/run1.bat
@@ -6,10 +6,8 @@ echo cd _work
 echo cd 53
 echo mkdir CIPMS
 echo cd CIPMS
-echo dir c:\build-agent\_work\53\a\
-echo dir c:\build-agent\_work\53\b\
-dir c:\build-agent\_work\53\s\
-del  c:\build-agent\_work\53\s\ 
-Y
+dir c:\build-agent\_work\53
+dir c:\build-agent\_work\53\a\
+dir c:\build-agent\_work\53\b\
 dir c:\build-agent\_work\53\s\
 dir c:\build-agent\_work\53\CIPMS\
